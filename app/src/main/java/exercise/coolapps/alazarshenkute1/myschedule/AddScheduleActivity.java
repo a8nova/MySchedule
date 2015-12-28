0b93f586c449be55007414df19b902d162992784
alazar <a8nova@gmail.com>
2015-12-27 23:36:57 -0800

    connected my app to parse

--- app/src/main/java/exercise/coolapps/alazarshenkute1/myschedule/AddScheduleActivity.java
+++ app/src/main/java/exercise/coolapps/alazarshenkute1/myschedule/AddScheduleActivity.java
@@ -1,7 +1,7 @@
 package exercise.coolapps.alazarshenkute1.myschedule;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
-
 
 public class AddScheduleActivity extends SingleFragmentActivity {
     @Override
@@ -9,5 +9,10 @@ public class AddScheduleActivity extends SingleFragmentActivity {
         return new AddScheduleFragment();
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+
+    }
 }
 
